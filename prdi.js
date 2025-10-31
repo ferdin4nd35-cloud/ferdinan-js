0296d6cef1a6ea6f05b1e92478dccddfde568df2
Ibrahimovic1970 <ahmad.ibrahimovic93@smk.belajar.id>
2025-10-31 13:55:16 +0700

    punya ferdinan sinaga

---
+++ prdi.js
@@ -0,0 +1,20 @@
+function nama(name) {
+    return name;
+}
+function umur(age) {
+    return age;
+}
+function alamat(address) {
+    return address;
+}
+function hobi(hobby) {
+    return hobby;
+}
+function sekolah(school) {
+    return school;
+}
+function kelas(classs) {
+    return classs;
+}
+document.write(`Perkenalkan nama saya ${nama("Ferdinand")}, umur saya ${umur(16)}, alamat saya di ${alamat("Cirebon")}, hobi saya adalah ${hobi("badminton")}, saya sedang sekolah di ${sekolah("SMK TI BAZMA")}, mengambil kelas ${kelas("X-SIJA")}. Terima kasih`);
+// code by ferdinand
---
+++ pro.js
@@ -0,0 +1,50 @@
+function name(nama) {
+  return nama;
+}
+function alamat(rumah) {
+  return rumah;
+}
+function hobi(hob) {
+  return hob;
+}
+function jurusan(jurus) {
+  return jurus;
+}
+function wa(no) {
+  return no;
+}
+function sekolah(seko) {
+  return seko;
+}
+function cita(cit) {
+  return cit;
+}
+
+function umur(um) {
+  return um;
+}
+document.write(`perkenalkan nama saya ${name("Payad")} umur saya ${umur(
+  15
+)} alamat saya di ${alamat("Bumi")} hobi saya adalah ${hobi(
+  "Apa aja yang penting asik"
+)}
+  saya sedang sekolah di ${sekolah("SMK TI BAZMA")} mengambil jurusan ${jurusan(
+  "SIJA"
+)} cita cita saya menjadi ${cita(
+  "Manusia Purba Premium Battle Pass"
+)} untuk informasi lebih lanjut bisa menghubungi saya di ${wa("0812121212")}
+  terima kasih`);
+document.write(`</br> </br>perkenalkan nama saya ${name(
+  "Payad"
+)}</br> umur saya ${umur(15)} </br>alamat saya di ${alamat(
+  "Bumi"
+)}</br> hobi saya adalah ${hobi("Apa aja yang penting asik")}</br>
+  saya sedang sekolah di ${sekolah(
+    "SMK TI BAZMA"
+  )}</br> mengambil jurusan ${jurusan(
+  "SIJA"
+)}</br> cita cita saya menjadi ${cita(
+  "Manusia Purba Premium Battle Pass"
+)}</br> untuk informasi lebih lanjut bisa menghubungi saya di ${wa(
+  "0812121212"
+)} Terimakasih`);
